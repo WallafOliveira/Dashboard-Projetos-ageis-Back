d992d221ef901d4b80a31d50f6bab59e3ac4cfb9
FelipeSizilio <135241083+FelipeSizilio@users.noreply.github.com>
2026-04-16 16:30:03 -0300

    suas alterações aqui

--- app/Models/Venda.php
+++ app/Models/Venda.php
@@ -7,12 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 class Venda extends Model
 {
     protected $table = 'vendas';
+
     protected $fillable = [
         'produto_id',
         'usuario_id',
         'quantidade',
         'preco_total',
     ];
+
+    protected $casts = [
+        'preco_total' => 'float',
+    ];
+
+    public function getTotalAttribute(): float
+    {
+        return (float) $this->preco_total;
+    }
 
     public function produto()
     {
